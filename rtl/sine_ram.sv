e24ffa2c5a986740430bdc63945c8521633ca22f
Karamoko27 <kara.2002@hotmail.fr>
2026-01-17 18:34:50 -0500

    sine ram and mem files

---
+++ rtl/sine_ram.sv
@@ -0,0 +1,21 @@
+module sine_ram (
+    input  wire         clk_i           ,// clock
+    input  wire         cen_i           ,// clock enable
+    input  wire [3:0]  addr_rd    ,   
+    output wire [7:0]  data_o        // 
+); 
+
+reg [7:0] rom [0:32767]; // 32K x 8-bit ROM
+reg [7:0] data;
+initial begin
+    $readmemh("sine.mem", rom);
+end
+always @(posedge clk_i) begin
+    if (cen_i) begin
+        data <= rom[addr_rd];
+    end
+end
+
+assign data_o = data;
+
+endmodule
