fbae5e435e45bed8f4d0e01b9d168a41cfcdb7c0
julien <julienyang12@gmail.com>
2026-01-18 09:57:09 -0500

    final donut!!

--- rtl/donut_rom.sv
+++ rtl/donut_rom.sv
@@ -7,7 +7,7 @@ module donut_rom (
     output wire [3:0]  data_o        // 
 ); 
 
-reg [3:0] rom [0:16895999]; // 60 frames × 281,600 = 16,896,000 x 4-bit ROM
+reg [3:0] rom [0:2111999]; // 30 frames × 70,400 = 2,112,000 x 4-bit ROM
 reg [3:0] data;
 initial begin
     $readmemh("donut_data.mem", rom);
